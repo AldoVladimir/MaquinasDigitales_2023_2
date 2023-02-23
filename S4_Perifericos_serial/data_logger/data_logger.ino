72848429ca70df659fb6f143793cb7a7799cd899
AldoVladimir <aldovladimir@ciencias.unam.mx>
2023-02-23 14:00:20 -0600

    SD logger

---
+++ S4_Perifericos_serial/data_logger/data_logger.ino
@@ -0,0 +1,43 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include "Adafruit_BMP280.h"
+
+//Loguear la info dle sensor barometrico y LDR
+#define LED_PIN 33
+#define LDR_PIN 35
+
+File readings;
+Adafruit_BMP280 bmp; //Objeto bmp
+
+char payload [80]; //payload de lecturas
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+  bmp.begin(0x76);
+  //Montarlo. Si falla, manda mensaje de error
+  if(!SD.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+}
+
+void loop() {
+
+  //Hacer el payload de lecturas de sensores
+  sprintf(payload,"%6.2f,%3.2f,%d",
+          bmp.readPressure(),
+          bmp.readTemperature(),
+          analogRead(LDR_PIN));
+
+  digitalWrite(LED_PIN,HIGH);
+    readings = SD.open("/lecturas_sensor.csv",FILE_APPEND);
+    readings.println(payload);
+    readings.close();
+    Serial.println(payload);
+  digitalWrite(LED_PIN,LOW);
+
+   delay(500);  
+
+}
