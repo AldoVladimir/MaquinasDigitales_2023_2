8a22b502472bf46180160df929fb078620758c59
AldoVladimir <aldovladimir@ciencias.unam.mx>
2023-05-30 16:22:12 -0600

    Forecast para una variable

--- S16_Forecast/prophet_Timestream.py
+++ S16_Forecast/prophet_Timestream.py
@@ -8,7 +8,6 @@ Created on Sat Oct  8 17:53:06 2022
 
 # %% Importar bibliotecas
 import awswrangler as wr
-#import boto3 
 import pandas as pd
 from prophet import Prophet
 from datetime import datetime,timedelta
@@ -16,44 +15,38 @@ from datetime import datetime,timedelta
 # %% Consultar fuentes en Amazon
 
 #Timestream
-query = 'SELECT * FROM "EmbebedCloud"."Axolote_all_devices" WHERE deviceID = \'Axolote_Aldo\' ORDER BY time DESC ' 
+# query = """select time,
+# 	  measure_value::double as temperatura_c
+# from atmosfera_axolote.axolote_oscar_qa
+# where measure_name = 'temp_C'
+# 	and time between ago(30d) and now()
+#order by time desc"""
+query = """select time,
+	  measure_value::double as temperatura_c
+from atmosfera_axolote.axolote_oscar_qa
+where measure_name = 'press_hPa'
+	and time between ago(30d) and now()
+order by time desc"""
+
 df = wr.timestream.query(query)
-# %% Preprocesamiento
-#La consulta de timestream está en formato 'wide'
-#Hay que cambiarla a formato 'Long'
-df = df.pivot(index='time', 
-                       columns='measure_name', 
-                       values= ['measure_value::double','measure_value::varchar']).dropna(axis = 1, thresh = 10)
-
-
-#Deshacermos de las columnas multinivel
-df.columns = df.columns.get_level_values(1)
-df = df.drop(columns=["device_Id","lat_deg","lat_long","long_deg","gps_date_time","mac_Id"])
-
-#Formatear columnas con su tipo de dato
-df["ligh_adim"] = pd.to_numeric(df["ligh_adim"],errors = "coerce")
-df["press_hPa"] = pd.to_numeric(df["press_hPa"],errors = "coerce")
-df["temp_C"] = pd.to_numeric(df["temp_C"],errors = "coerce")
-df.index = pd.to_datetime(df.index)
-
-
 # %% Previsualizar
 
-variable = "ligh_adim"
-#df.plot(y = "press_hPa", use_index = True)
+#variable = "ligh_adim"
+df.plot(y = 1, x = 0, xlim = [datetime(2023,5,22),datetime(2023,5,30)])
 #Subsetting para tener información manejable
 #Resampling para tener una frecuencia de discretización constante
 #Rolling para eliminar transiciones de alta frecuencia
-df_subset = df[df.index>(datetime.today()-timedelta(days = 8))]
+df_subset = df[df.iloc[:,0]>(datetime.today()-timedelta(days = 8))]
+df_subset.set_index('time',inplace= True)
 df_resamp = df_subset.resample('10min').mean().interpolate()
-df_rolling = df_resamp.rolling(3).mean()
+df_rolling = df_resamp.rolling(5).mean()
 
-df_subset.plot(y = variable, use_index = True)
-df_resamp.plot(y = variable, use_index = True)
-df_rolling.plot(y = variable, use_index=True )
+df_subset.plot(y = 0, use_index = True)
+df_resamp.plot(y = 0, use_index = True)
+df_rolling.plot(y = 0, use_index = True )
 # %%Prophet fit
 df_prophet = pd.DataFrame()
-df_prophet["y"] = df_rolling[variable]
+df_prophet["y"] = df_rolling.iloc[:,0]
 df_prophet["ds"] = df_rolling.index.tz_localize(None)
 
 m = Prophet(changepoint_prior_scale=0.001).fit(df_prophet)
